ee6ffac5f8a90ce585b75d0bbefd4b40ce0118a8
Kevin Castro <castrok.c11@hotmail.com>
2020-03-26 16:48:19 -0500

    fix creating the footer

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,4 +1,4 @@
-<footer class="my-footer">
+<footer class="main-footer">
 
     <strong>Copyright &copy; 2017 <a href="https://
     www.tutorialesatualcance.com" target="_blank">Tutoriales a tu alcance</a>.
